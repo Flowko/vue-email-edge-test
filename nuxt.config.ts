4500e49dcb27b9de9999dccca1ea00a2d8934d43
Younes Barrad <younesbarraddev@gmail.com>
2023-12-15 22:12:25 +0100

    chore: bump versions and fix missing props

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  nitro: { preset: "cloudflare-pages" },
   modules: ["@vue-email/nuxt"],
 })
--- server/api/email.ts
+++ server/api/email.ts
@@ -1,5 +1,10 @@
 import { useCompiler } from "#vue-email"
 
 export default defineEventHandler(async (event) => {
-  return await useCompiler("Template.vue", {})
+  return await useCompiler("Template.vue", {
+    props: {
+      heading: "Hello World",
+      previewText: "This is a preview text",
+    }
+  })
 })
