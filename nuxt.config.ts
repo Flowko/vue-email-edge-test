4c152cad4419c834c4864468efdfc7a009830fbd
Younes Barrad <younesbarraddev@gmail.com>
2024-02-01 12:03:54 +0100

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@vue-email/nuxt"],
-  // nitro: {
-  //   preset: 'vercel-edge'
-  // }
+  nitro: {
+    preset: 'vercel-edge'
+  }
 })
